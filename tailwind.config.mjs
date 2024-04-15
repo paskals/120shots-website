1255cb1d26642ba3bba10b50339c939f2cfd20d6
Paskal S <djpascimix@gmail.com>
2024-04-15 22:03:32 +0200

    Various improvements
    Transition style for dark/light mode
    404 page fix
    Authors page fix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,16 +4,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        blue: "#1fb6ff",
-        purple: "#7e5bef",
-        pink: "#ff49db",
-        orange: "#ff7849",
-        green: "#13ce66",
-        yellow: "#ffc82c",
         "gray-dark": "#273444",
-        gray: "#8492a6",
         "gray-light": "#d3dce6",
-        background: "rgb(245,245,245)",
         "dark-background": "rgb(34,33,37)",
       },
       fontFamily: {
