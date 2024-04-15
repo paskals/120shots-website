7ab55fbf8f0dd51e6d9791754d3afbba83f32f1e
Paskal S <djpascimix@gmail.com>
2024-04-15 21:24:37 +0200

    Better tag layout, created film layout and film routes

--- src/scripts/getrandomimage.js
+++ src/scripts/getrandomimage.js
@@ -7,16 +7,12 @@ function handlePageLoad() {
   imageElements.forEach((element) => {
     let images = JSON.parse(element.dataset.images);
     let alt = JSON.parse(element.dataset.alt); // Parse the alt texts
-    let width = element.dataset.width; // Get the width
-    let height = element.dataset.height; // Get the height
 
     // Combine the images and alt texts into a single array of objects
     let items = images.map((image, index) => {
       return {
         image: image,
         alt: alt[index],
-        width: width,
-        height: height,
       };
     });
 
@@ -32,8 +28,6 @@ function handlePageLoad() {
     let firstItem = items[0];
     imgElement.src = firstItem.image;
     imgElement.alt = firstItem.alt;
-    imgElement.width = firstItem.width; // Set the width
-    imgElement.height = firstItem.height; // Set the height
     imgElement.classList.remove("hidden"); // Remove the 'hidden' class
   });
 }
